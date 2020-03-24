8a636ec6b6d43eeeeca9f09d116615ff2c75c25b
asifRaeesIqbal <asif.iqbal898@hotmail.com>
2020-03-24 22:04:03 +0000

    Initial Scaffold Commit

---
+++ moj-AIqbal1-tech-challenge/src/main/java/com/moj/challenge/MojAIqbal1TectChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.moj.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojAIqbal1TectChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojAIqbal1TectChallengeApplication.class, args);
+	}
+
+}
---
+++ moj-AIqbal1-tech-challenge/src/test/java/com/moj/challenge/MojAIqbal1TectChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moj.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojAIqbal1TectChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
