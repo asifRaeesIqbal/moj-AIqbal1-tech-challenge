0f590b14987ad7821b86330f62510c153eb9fcd5
Asif Iqbal <iqbala79@hotmail.com>
2020-03-28 21:07:05 +0000

    Updated web controller

--- moj-AIqbal1-tech-challenge/src/main/java/com/moj/challlenge/web/CheckDateController.java
+++ moj-AIqbal1-tech-challenge/src/main/java/com/moj/challlenge/web/CheckDateController.java
@@ -2,10 +2,13 @@ package com.moj.challlenge.web;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.moj.challenge.service.DateTimeValidityCheckerFacade;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,22 +19,17 @@ import io.swagger.annotations.ApiResponses;
 @Api(tags = "Valid Date Checker API")
 public class CheckDateController {
 
-	
+	@Autowired
+	private DateTimeValidityCheckerFacade checkDateController;
+
 	@ApiOperation(value = "Checks if the date passed in is valid.")
-	@ApiResponses(value = {
-	    @ApiResponse(code = 200, message = "Successfully checked the date."),
-	    @ApiResponse(code = 401, message = "You are not authorized to get this information."),
-	})
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully checked the date."),
+			@ApiResponse(code = 401, message = "You are not authorized to get this information."), })
 	@GetMapping("/callcenter/availability/date/check")
-    public boolean getStatus(@RequestParam("dateTime")
-    								@DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME) 
-    								LocalDateTime dateTime) throws Exception{
+	public boolean getStatus(
+			@RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime)
+			throws Exception {
 
-        boolean isValidDate = false;
-
-
-
-        return isValidDate;
-
-}
+		return checkDateController.isDateTimeValid(dateTime);
+	}
 }
